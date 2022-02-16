fd4a2fe22911de82c06ab34ab73ccb60364a5b01
Justin Conger <jhconger@GMAIL.COM>
2022-02-16 16:06:44 -0600

    completed 1b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,29 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i++ + " ");
-
-        }
-
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i++ + " ");
+//
+//        }
+//        System.out.println();
+//        int count = 0;
+//        do {
+//            System.out.println(count);
+//            count = count + 2;
+//        } while(count <= 100);
+//    }
+//        System.out.println();
+//        int count = 100;
+//        do {
+//            System.out.println(count);
+//            count = count -5;
+//        } while(count >= -10);
+//    }
+//        System.out.println();
+//        long count = 2;
+//        do {
+//            System.out.println(count);
+//            count *= count;
+//        } while (count < 1000000);
     }
 }
