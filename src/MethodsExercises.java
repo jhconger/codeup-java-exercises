3bcbf49001f58b81ce20598f12f9b02be51fd314
Justin Conger <jhconger@GMAIL.COM>
2022-02-18 16:14:07 -0600

    completed first Bonus.

---
+++ src/MethodsExercises.java
@@ -0,0 +1,63 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        multiply(2, 6);
+        add(3, 3);
+        subtract(12, 6);
+        divide(50, 10);
+        modulus(2, 2);
+
+    }
+
+    public static int add(int x, int y) {
+        System.out.println(x + y);
+        return x + y;
+    }
+
+    public static int subtract(int x, int y) {
+        System.out.println(x - y);
+        return x - y;
+    }
+
+//    public static int multiply(int x, int y) {
+//        int result = 0;
+//        for (int i = 1; i <= y; i++) {
+//            result += x;
+//        }
+//        System.out.println(result);
+//        return result;
+//    }
+
+    static void multiply(int x, int y) {
+        int result = product(x, y);
+        System.out.println(result);
+    }
+
+    static int product(int x, int y) {
+        // if x is less than
+        // y swap the numbers
+        if (x < y)
+            return product(y, x);
+            // iteratively calculate
+            // y times sum of x
+        else if (y != 0)
+            return (x + product(x, y - 1));
+            // if any of the two numbers is
+            // zero return zero
+        else
+            return 0;
+    }
+
+    public static float divide(int x, int y) {
+        if (y == 0) {
+            System.out.println("Error, cannot divide by zero");
+        }
+        float result = ((float) x) / y;
+        System.out.println(result);
+        return result;
+    }
+
+    public static int modulus(int x, int y) {
+        System.out.println(x % y);
+        return x % y;
+    }
+}
