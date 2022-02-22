baf8b29d32627e2c2618b91d29d043c2b7a6457e
Justin Conger <jhconger@GMAIL.COM>
2022-02-22 12:15:28 -0600

    reworked get int.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,10 +8,11 @@ public class MethodsExercises {
 //        subtract(12, 6);
 //        divide(50, 10);
 //        modulus(2, 2);
-////        getInteger(1, 10);
+        getInteger(1, 10);
 //        factor(8);
     }
-//
+
+    //
 //    public static int add(int x, int y) {
 //        System.out.println(x + y);
 //        return x + y;
@@ -66,48 +67,61 @@ public class MethodsExercises {
 //    }
 //
 //
-////    public static int getInteger(int min, int max) {
-////        Scanner scanner = new Scanner(System.in);
-////        System.out.println("Enter a number between 1 and 10: ");
-////        int userNum = scanner.nextInt();
-////        if (userNum < 1) {
-////            System.out.println("Your number is too low. Let's go higher.");
-////            return getInteger(min, max);
-////        } else if (userNum > 10) {
-////            System.out.println("Your number is too high. Let's go lower.");
-////            return getInteger(min, max);
-////        } else {
-////            return userNum;
-////        }
-////    }
-
 //
-    static {
+//
+    public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
-        String choice = "y";
-
-        while (choice.equalsIgnoreCase("y")) {
-            System.out.println("Enter a number between 1 and 20: ");
-            int num = scanner.nextInt();
-            if (num < 1) {
-                System.out.println("Your number is too low. Let's go higher.");
-            } else if (num > 20) {
-                System.out.println("Your number is too high. Let's go lower.");
-            } else {
-                int i = 0;
-                long factorial = 1;
-
-                for (i = 1; i <= num; ++i) {
-                    // factorial = factorial * i;
-                    factorial *= i;
-                }
-                System.out.printf("Factorial of %d = %d", num, factorial);
-                System.out.print("\nContinue? (y/n): ");
-                choice = scanner.next();
-                System.out.println();
-            }
+        System.out.println("Enter a number between 1 and 10: ");
+        int userNum = scanner.nextInt();
+        if (userNum < min) {
+            System.out.println("Your number is too low. Let's go higher.");
+            return getInteger(min, max);
+        } else if (userNum > max) {
+            System.out.println("Your number is too high. Let's go lower.");
+            return getInteger(min, max);
+        } else {
+            return userNum;
         }
     }
+}
+//        if (userNum < 1) {
+//            System.out.println("Your number is too low. Let's go higher.");
+//            return getInteger(min, max);
+//        } else if (userNum > 10) {
+//            System.out.println("Your number is too high. Let's go lower.");
+//            return getInteger(min, max);
+//        } else {
+//            return userNum;
+//        }
+//    }
+
+//
+//    static {
+//        Scanner scanner = new Scanner(System.in);
+//        String choice = "y";
+//
+//        while (choice.equalsIgnoreCase("y")) {
+//            System.out.println("Enter a number between 1 and 20: ");
+//            int num = scanner.nextInt();
+//            if (num < 1) {
+//                System.out.println("Your number is too low. Let's go higher.");
+//            } else if (num > 20) {
+//                System.out.println("Your number is too high. Let's go lower.");
+//            } else {
+//                int i = 0;
+//                long factorial = 1;
+//
+//                for (i = 1; i <= num; ++i) {
+//                    // factorial = factorial * i;
+//                    factorial *= i;
+//                }
+//                System.out.printf("Factorial of %d = %d", num, factorial);
+//                System.out.print("\nContinue? (y/n): ");
+//                choice = scanner.next();
+//                System.out.println();
+//            }
+//        }
+//    }
 
 
 
@@ -121,32 +135,34 @@ public class MethodsExercises {
 //        Use the .random method of the java.lang.Math class to generate random numbers.
 
 
-     static  {
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
-        boolean keepRolling = true;
-         String choice = "y";
-         System.out.println("Enter the number of sides for your pair of dice:");
-         int sides = scanner.nextInt();
-
-        do {
-            System.out.println("Would you like to roll your dice? (y/n): ");
-            if (scanner.next().equalsIgnoreCase("y")) {
-//                int n = rand.nextInt(sides * 2);
-//                this resulted in rolls of 1 or 0 sometimes which is not possible with di.
-                int dice1=(int)(Math.random()*sides+1);
-                int dice2=(int)(Math.random()*sides+1);
-                int sum= dice1 + dice2;
-                System.out.println("You rolled " + sum);
-            } else {
-                System.out.println("Let's play again sometime.");
-                keepRolling = false;
-            }
-        } while(keepRolling);
-                choice = scanner.next();
-     }
-}
-
+//     static  {
+//        Scanner scanner = new Scanner(System.in);
+//        Random rand = new Random();
+//        boolean keepRolling = true;
+//         String choice = "y";
+//         System.out.println("Enter the number of sides for your pair of dice:");
+//         int sides = scanner.nextInt();
+//
+//        do {
+//            System.out.println("Would you like to roll your dice? (y/n): ");
+//            if (scanner.next().equalsIgnoreCase("y")) {
+////                int n = rand.nextInt(sides * 2);
+////                this resulted in rolls of 1 or 0 sometimes which is not possible with di.
+//                int dice1=(int)(Math.random()*sides+1);
+//                int dice2=(int)(Math.random()*sides+1);
+//                int sum= dice1 + dice2;
+//                System.out.println("You rolled " + sum);
+//            } else {
+//                System.out.println("Let's play again sometime.");
+//                keepRolling = false;
+////                break;
+//            }
+//        } while(keepRolling);
+//                choice = scanner.next();
+//
+//     }
+//}
+//
 
 
 
