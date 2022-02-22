87d2373cd7ab07779a6f99b24d1f04a901acb0db
Justin Conger <jhconger@GMAIL.COM>
2022-02-22 13:41:25 -0600

    reformatted input class.

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,7 +6,7 @@ public class Input {
     private Scanner scanner;
 
     public Input() {
-        scanner = new Scanner(System.in);
+        this.scanner = new Scanner(System.in);
     }
 
     public String getString() {
@@ -20,70 +20,37 @@ public class Input {
     }
 
     public int getInt(int min, int max) {
-        System.out.println("Enter a number in the range of " + min + " and " + max + ": ");
-        int userNum = Integer.parseInt(getString());
-        try {
-            int parsedNum1 = userNum;
-        } catch (NumberFormatException e) {
-            System.out.println("That number is not in range. Try again.");
-            return getInt(min, max);
-        }
-        if (userNum < min || userNum > max) {
-            System.out.println("Try another number");
-            getInt(min, max);
-        }
+        int userNum;
+        do {
+            System.out.println("Please enter a number between " + min + " and " + max + "; ");
+            userNum = scanner.nextInt();
+        } while (userNum > max || userNum < min);
         return userNum;
     }
-//    public static int getInt(int min, int max) {
-//
-//        int userNum = scanner.nextInt();
-//        if (userNum < min {
-//            System.out.println("Your number is too low. Let's go higher.");
-//            return getInteger(min, max);
-//        } else if (userNum > max) {
-//            System.out.println("Your number is too high. Let's go lower.");
-//            return getInteger(min, max);
-//        } else {
-//            return userNum;
-//        }
-//    }
 
-    int getInt() {
-        System.out.println("Enter a number without limits: ");
-        try {
-            return Integer.valueOf(getString());
-        } catch (NumberFormatException e) {
-            System.out.println("That isn't an acceptable number. Try again.");
-            return getInt();
-        }
+    public int getInt() {
+        System.out.println("Please enter a number: ");
+        return scanner.nextInt();
     }
 
-    double getDouble(double min, double max) {
-        System.out.println("Enter a double (number without decimals) in the range of " + min + " and " + max + ": ");
-//        double userDouble = scanner.nextDouble();
-        double parsedNum = Double.valueOf(getString());
-        try {
-            if (parsedNum < min || parsedNum > max) {
-                System.out.println("Try another number");
-                getDouble(min, max);
-            }
-            return parsedNum;
-        } catch (NumberFormatException e) {
-            System.out.println("That is not an acceptable number. Try again.");
-            return getDouble(min, max);
-        }
+    public double getDouble(double min, double max) {
+        double userNum;
+        do {
+            System.out.println("Enter a double (number without decimals) in the range of " + min + " and " + max + ": ");
+            userNum = scanner.nextDouble();
+        } while (userNum > max || userNum < min);
+        return userNum;
     }
 
     public double getDouble() {
         System.out.println("Enter a double without limits: ");
-//        return scanner.nextDouble();
+        double userNum = scanner.nextDouble();
         try {
-            return Double.valueOf(getString());
+            return userNum;
         } catch (NumberFormatException e) {
             System.out.println("That is not an acceptable double. Try again.");
             return getDouble();
         }
-
     }
 
     public static void main(String[] args) {
@@ -96,7 +63,7 @@ public class Input {
         System.out.println("Your number = " + yourNumber);
         int noLimit = input.getInt();
         System.out.println("You have entered a no limit integer = " + noLimit);
-        double userDouble = input.getDouble(-1000.0, 1000.0);
+        double userDouble = input.getDouble(-1000, 1000);
         System.out.println("The double you entered = " + userDouble);
         double noLimitDouble = input.getDouble();
         System.out.println("The no limit double you entered = " + noLimitDouble);
