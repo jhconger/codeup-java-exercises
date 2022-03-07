768154e299cad0a865bff0d4bab7cd343fd72895
Justin Conger <jhconger@GMAIL.COM>
2022-03-06 20:11:34 -0600

    ShapesTest complete

---
+++ src/ArraysExercises.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+
+    public static void main(String[] args) { //method
+//        int[] numbers = {1, 2,3, 4, 5};
+//it will not print out until we wrap with Arrays.toString
+//        System.out.println(Arrays.toString(numbers));
+
+        String[] person = new String[]{"John", "George", "Amanda"};
+
+        System.out.println(Arrays.toString(people));
+
+    }
+}
